1593420c8dd201ecd95cd28ff2a11cc7511c923c
Såndræ <Sndr>
2017-02-04 00:37:32 +0100

    lagt til filer?

---
+++ BachelorHTML/public_html/JavaScript/cancel-modal.js
@@ -0,0 +1,13 @@
+(function ()
+{
+    var modal = document.getElementById("id01");
+
+    window.onclick = function (event)
+    {
+        if (event.target === modal)
+        {
+            modal.style.display = "none";
+        }
+    };
+}
+);
