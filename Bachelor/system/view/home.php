77e12d39da052854d4b105eb2ca3e679964c36cb
rogerk <rog_kol03@hotmail.com>
2017-02-10 12:36:41 +0100

    Tabell i home siden

--- Bachelor/system/view/home.php
+++ Bachelor/system/view/home.php
@@ -1,7 +1,9 @@
 
                 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                
-                    <br><br><br><br>
+                    <br> <br><br>        
+ <div class="col-sm-3 col-md-4">
+     
+
                     <div class="table-responsive">
                         <table class="table table-striped">
                             <thead>
@@ -10,7 +12,7 @@
                                     <th>Navn</th>
                                     <th>Etternavn</th>
                                     <th>Stilling</th>
-                                    <th>Alder</th>
+                                    
                                 </tr>
                             </thead>
                             <tbody>
@@ -19,26 +21,100 @@
                                     <td>Sondre</td>
                                     <td>Fjærtoft</td>
                                     <td>CEO</td>
-                                    <td>24</td>
+                                    
                                 </tr>
                                 <tr>
                                     <td>2</td>
                                     <td>Roger</td>
                                     <td>Kolseth</td>
                                     <td>Vaskehjelp</td>
-                                    <td>12</td>
+                                    
                                 </tr>
                                 <tr>
                                     <td>3</td>
                                     <td>Ole</td>
                                     <td>Lid aka Lidern</td>
                                     <td>Kaffegutt</td>
-                                    <td>12</td>
+                                    
                                 </tr>
                             </tbody>
                         </table>
                         
                     </div>
+                    </div>
+                        <div class="col-sm-3 col-md-4">
+                            <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Navn</th>
+                                    <th>Etternavn</th>
+                                    <th>Stilling</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Sondre</td>
+                                    <td>Fjærtoft</td>
+                                    <td>CEO</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Roger</td>
+                                    <td>Kolseth</td>
+                                    <td>Vaskehjelp</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Ole</td>
+                                    <td>Lid aka Lidern</td>
+                                    <td>Kaffegutt</td>
+                                    
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    <div class="col-sm-3 col-md-4">
+                            <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Navn</th>
+                                    <th>Etternavn</th>
+                                    <th>Stilling</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Sondre</td>
+                                    <td>Fjærtoft</td>
+                                    <td>CEO</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Roger</td>
+                                    <td>Kolseth</td>
+                                    <td>Vaskehjelp</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Ole</td>
+                                    <td>Lid aka Lidern</td>
+                                    <td>Kaffegutt</td>
+                                    
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+
 
 
                     <!-- HER KOMMER INNHOLDET>   -->                
