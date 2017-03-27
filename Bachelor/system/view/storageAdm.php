3188eff15304e2f6916fb29b33f7852e40c8a9c8
rogerkolseth <Kolseth@Rogers-MacBook-Pro.local>
2017-03-27 03:29:13 +0200

    låser header ved scrolling

--- Bachelor/system/view/storageAdm.php
+++ Bachelor/system/view/storageAdm.php
@@ -1,6 +1,6 @@
 <?php require("view/header.php"); ?>
 
-<div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main">
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 
     <div class="container">
 
