c2d0a6376a4d640178e5e1e732b67c6fb909754d
rogerk <rog_kol03@hotmail.com>
2017-04-01 14:57:30 +0200

    returnsiden viser kun et lager om du kun har tilgang til ett

--- Bachelor/system/view/returnSingle.php
+++ Bachelor/system/view/returnSingle.php
@@ -2,4 +2,273 @@
 
 
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-    <div class="container"></div>
+
+    <div class="container">
+        <div class="col-sm-3 col-sm-offset-1 col-md-10 col-md-offset-1 form-group">
+            <form id="returnProducts" action="?page=returnProduct" method="post">
+                <div class="col-sm-3 col-md-4">
+                    <label>Uttak fra: </label><div id="returnRestrictionContainer"></div>
+                </div>
+
+                <br><br><br><br>
+
+        <div id="returnProductContainer">
+            
+
+            <!-- Viser Product som er valgt i FRA lager -->
+
+
+        </div>  
+                
+        
+        <br><br><br>
+
+        <div>
+            <table class="table table-bordered table-striped table-responsive" id="commentContainer">
+
+                <tr>
+                    <th>Kundenr:   </th> 
+                    <td><input name="customerNumber" required="required" form="returnProducts" type="int" value=""/></td> 
+                </tr>  
+                <tr>
+                    <th>kommentar:  </th>
+                    <td><input name="returnComment" required="required" form="returnProducts" type="text" value=""/></td>
+                </tr>
+
+            </table>
+        </div>   
+        
+        <div>
+            <table class="table table-bordered table-striped table-responsive" id="returnQuantityContainer">
+            
+                <!-- Lar deg velge antall enheter -->
+
+            </table>
+
+            
+            <input form="returnProducts" type="hidden" id="date" name="date">
+            
+            <button form="returnProducts" type="submit" class="btn btn-default" id="returnButton">Overfør</button>
+            <p id="errorMessage"></p>
+        </div> 
+        </form>
+        </div>
+    </div>        
+
+</div> 
+
+<script id="returnQuantityTemplate" type="text/x-handlebars-template">
+{{#each product}} 
+    <tr class="selectQuantity">
+        <th>Produkt:   </th>
+        <td>{{productName}}</td>
+        <input name="returnProductID[]" id="{{productID}}" form="returnProducts" type="hidden" value="{{productID}}"/>
+        <th>Antall:</th>
+        <td><input name="returnQuantity[]" form="returnProducts" required="required" type="number" min="1" max="1000" value="" autocomplete="off"/></td>  
+        
+        <td>
+            <button class="remove" data-toggle="tooltip" 
+                style="appearance: none;
+                -webkit-appearance: none;
+                -moz-appearance: none;
+                outline: none;
+                border: 0;
+                background: transparent;
+                display: inline;">
+                <span class="glyphicon glyphicon-remove" style="color: red"></span>
+            </button>
+        </td>    
+        
+    </tr>
+{{/each}}  
+</script>
+
+<script id="returnProductTemplate" type="text/x-handlebars-template">
+    <br>  
+    {{#each storageProduct}} 
+    <button data-id="{{productID}}" class="btn btn-default product">{{productName}}</button>
+    {{/each}} 
+</script>
+
+<!-- Display storages in drop down meny Template -->
+<script id="returnRestrictionTemplate" type="text/x-handlebars-template">
+{{#each transferRestriction}}    
+    {{storageName}}
+   <input type="hidden" name="toStorageID" form="returnProducts" value="{{storageID}}">
+{{/each}}
+        
+</script>
+
+<!-- Get storage information with user restriction -->
+<script>
+    $('#returnButton').hide(); // hides transferbutton  
+    $('#commentContainer').hide();
+    $(function () {
+        $.ajax({
+            type: 'GET',
+            url: '?page=getTransferRestriction',
+            dataType: 'json',
+            success: function (data) {
+                returnRestrictionTemplate(data);
+            }
+        });
+    });
+</script>
+
+
+<!-- Display storages in drop down meny Template -->
+<script>
+    function returnRestrictionTemplate(data) {
+        var rawTemplate = document.getElementById("returnRestrictionTemplate").innerHTML;
+        var compiledTemplate = Handlebars.compile(rawTemplate);
+        var transferRestrictionGeneratedHTML = compiledTemplate(data);
+
+        var transferContainer = document.getElementById("returnRestrictionContainer");
+        transferContainer.innerHTML = transferRestrictionGeneratedHTML;
+
+    }
+</script>
+
+<!-- Get the selected storage, and POST this to retrive inventory-->
+<script>
+
+    $(function () {
+        $.ajax({
+            type: 'GET',
+            url: '?page=getStorageProduct',
+            dataType: 'json',
+            success: function (data) {
+                returnProductTemplate(data);
+            }
+        });
+    });
+</script>
+
+<!-- Display products in storage Template -->
+<script>
+    function returnProductTemplate(data) {
+        var rawTemplate = document.getElementById("returnProductTemplate").innerHTML;
+        var compiledTemplate = Handlebars.compile(rawTemplate);
+        var transferProductGeneratedHTML = compiledTemplate(data);
+
+        var productContainer = document.getElementById("returnProductContainer");
+        productContainer.innerHTML = transferProductGeneratedHTML;
+    }
+</script>
+
+<!-- Get productID from selected ID -->
+<script>
+                            
+    $(function POSTselectedProduct() {
+
+        $('#returnProductContainer').delegate('.product', 'click', function () {
+        var givenProductID = $(this).attr('data-id');
+              if( $('#'+givenProductID).length )   
+            {
+                return false;
+            } else {
+
+
+            $.ajax({
+                type: 'POST',
+                url: '?page=getProductByID',
+                data: {givenProductID: givenProductID},
+                dataType: 'json',
+                success: function (data) {
+
+                    returnQuantityTemplate(data);
+                    $('#commentContainer').show();
+                    $('#returnButton').show();
+                }
+            });
+            return false;
+
+            }
+        });
+    });
+</script> 
+
+<script>
+    function returnQuantityTemplate(data) {
+        var rawTemplate = document.getElementById("returnQuantityTemplate").innerHTML;
+        var compiledTemplate = Handlebars.compile(rawTemplate);
+        var transferProductGeneratedHTML = compiledTemplate(data);
+
+        var transferContainer = document.getElementById("returnQuantityContainer");
+        transferContainer.innerHTML += transferProductGeneratedHTML;
+        
+    }
+</script>
+
+
+<script>
+    $(function POSTtransferProducts() {
+
+        $('#returnProducts').submit(function () {
+            var url = $(this).attr('action');
+            var data = $(this).serialize();
+
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: data,
+                dataType: 'json',
+                error: function () {
+                    var $displayUsers = $('#errorMessage');
+                    $displayUsers.empty().append("Kunne ikke overføre");
+                },
+                success: function (data) {
+                    $('.product').remove();
+                    $('.selectQuantity').remove();
+                    $('#errorMessage').remove();
+                    updateReturn();
+                }
+            });
+            return false;
+        });
+    });
+</script>
+
+<script> 
+function updateReturn() {                            
+$('#returnButton').hide();// hides transferbutton 
+$('#commentContainer').hide();
+$(function () {
+    $.ajax({
+        type: 'GET',
+        url: '?page=getTransferRestriction',
+        dataType: 'json',
+        success: function (data) {
+            returnRestrictionTemplate(data);
+            }  
+        });
+    });
+}
+</script>
+
+<script>
+Date.prototype.yyyymmdd = function() {
+   var yyyy = this.getFullYear();
+   var mm = this.getMonth() < 9 ? "0" + (this.getMonth() + 1) : (this.getMonth() + 1); // getMonth() is zero-based
+   var dd  = this.getDate() < 10 ? "0" + this.getDate() : this.getDate();
+   return "".concat(yyyy).concat(mm).concat(dd);
+  };
+
+var d = new Date();
+document.getElementById("date").value  = d.yyyymmdd();
+
+</script>
+
+<!-- remove product modal -->
+<script>
+    $(function POSTdeleteStorageModal() {
+
+        $('#returnQuantityContainer').delegate('.remove', 'click', function () {
+           var $tr = $(this).closest('tr');        
+          
+           $tr.fadeOut(150, function() {
+            $(this).remove();
+           });       
+        });
+    });
+</script> 
